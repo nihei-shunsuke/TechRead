a0301a4f238634a03d79b9a751acc09bab116df1
Mana Kinoshita <73377985+HappyMana@users.noreply.github.com>
2023-04-03 16:42:44 +0900

    イベント作成ページをリクエストするところまで作成

--- src/js/event/create-event.js
+++ src/js/event/create-event.js
@@ -1,12 +1,55 @@
-function btn1(){    
+// イベント作成のPOST関数
+function create_event() {
     // 入力フォームの内容を取得
-    const title = document.getElementById('title').value;
-    const word = document.getElementById('word').value;
+    const event_name = document.getElementById('event_name').value;
+    const book_name = document.getElementById('book_name').value;
+    const members = document.getElementsByClassName('member');
+    let member_list = [];
+    for(let i = 0; i < members.length; i++) {
+        member_list.push(members.item(i).value);
+    }
+
     // 入力内容を出力
-    console.log(title);
-    console.log(word);
+    console.log(event_name);
+    console.log(book_name);
+    console.log(member_list);
+
+    // 入力値をリクエスト
+    const parameter = {
+        method:'POST',
+        mode:'no-cors',
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify({
+            event_name: event_name,
+            book_name: book_name,
+            member_list: member_list,
+        })
+    };
+    const url = 'http://localhost:8080/create-event';
+
+    fetch(url, parameter).then((response) => {
+        console.log(response);
+    })
+    .catch(err => {
+        console.error(err);
+    })
 }
 
-const btn = document.getElementById('btn');
+const create_btn = document.getElementById('create-btn');
+create_btn.addEventListener('click', create_event, false);
 
-btn.onclick = btn1;
+// メンバーを追加するフォームを追加する関数
+function add_member_form() {
+    const newForm = document.createElement('input');
+    newForm.type = 'text';
+    newForm.className = 'member';
+
+    const br = document.createElement('br');
+    document.getElementById('add-member').appendChild(br);
+    document.getElementById('add-member').appendChild(newForm);
+};
+
+const add_member_button = document.getElementById('add-member-button');
+add_member_button.onclick = add_member_form;
