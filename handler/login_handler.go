e25a9130c59ca338b3ac6832639dd58d854b5344
poteto <poteto054@gmail.com>
2023-04-03 15:41:51 +0900

    ログインの修正

--- handler/login_handler.go
+++ handler/login_handler.go
@@ -22,16 +22,21 @@ func LoginHandler(w http.ResponseWriter, req *http.Request) {
 		http.Error(w, "fail internal email \n", http.StatusInternalServerError)
 		return
 	}
-	rows := database.DB.QueryRow("SELECT email, password FROM users WHERE email = ?", reqUserData.Email)//存在する場合
-	if row == sql.ErrNoRows {
+	rows,err := database.DB.Query("SELECT email, password FROM users WHERE email = ?", reqUserData.Email)//存在する場合
+	if err != nil {
 		http.Error(w, "fail internal email \n", http.StatusInternalServerError)
 		return
 	}
-	var reqUserData, userRecord model.User
-	err := rows.Scan(&userRecord.UserName,&userRecord.Email,&userRecord.Password)
-	if err != nil {
-		fmt.Println("データの読み込みに失敗しました",err)
-		return
+	fmt.Println(rows)
+
+	var userRecord model.User
+	for rows.Next() {
+		err = rows.Scan(&userRecord.Email,&userRecord.Password)
+		if err != nil {
+			fmt.Println("データの読み込みに失敗しました",err)
+			return
+		}
+		fmt.Println(userRecord.Email,userRecord.Password)
 	}
 	if reqUserData.Password != userRecord.Password {
 		fmt.Println("パスワードが違います")
