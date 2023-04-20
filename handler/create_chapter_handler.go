61cc0a6070d8bc7021f0a33dfa0daa630c4a9f97
poteto <poteto054@gmail.com>
2023-04-20 15:45:00 +0900

    チャプター作成機能追加

--- handler/create_chapter_handler.go
+++ handler/create_chapter_handler.go
@@ -1,10 +1,50 @@
 package handler
 
 import (
-	"io"
 	"net/http"
+	"TechRead/model"
+	"encoding/json"
+	"fmt"
+	"strconv"
+	"TechRead/database"
 )
 
 func CreateChapterHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "CreateChapterHandler\n")
+	var reqChapterData model.Chapter
+	var resState model.ResInfo
+	var userRecord model.User
+	if err := json.NewDecoder(req.Body).Decode(&reqChapterData); err != nil {
+		fmt.Println(err)
+		http.Error(w, "fail to decode json\n", http.StatusBadRequest)
+		ResFail(resState)
+		json.NewEncoder(w).Encode(resState)
+		return
+	}
+	fmt.Println(reqChapterData)
+	const sqlStr = `
+		insert into chapters (venue,event_id,content) values
+		(?,?,?);
+	`
+	cookies := req.Cookies()
+	if cookies != nil{
+		for _, c := range cookies {
+			ID,_ := strconv.ParseInt(c.Value, 10, 64)
+			fmt.Println(ID)
+			_ = database.DB.QueryRow("SELECT user_id FROM users WHERE user_id = ?", ID).Scan(&userRecord.UserID)
+		}
+		result, err := database.DB.Exec(sqlStr, reqChapterData.Venue, 1,reqChapterData.Content)
+		if err != nil {
+			fmt.Println(err)
+			http.Error(w, "fail internal exec \n", http.StatusInternalServerError)
+			ResFail(resState)
+			json.NewEncoder(w).Encode(resState)
+			return
+		}
+		id,_ := result.LastInsertId()
+		_, err = database.DB.Exec("INSERT INTO chapters (chapter_num (?)", id,)
+		resState.ResState = "success"
+		resState.UserID = userRecord.UserID
+		fmt.Println(id)
+		json.NewEncoder(w).Encode(resState)
+	}
 }
--- model/chapter.go
+++ model/chapter.go
@@ -8,7 +8,7 @@ type Chapter struct {
 	Venue         string    `db:"venue" form:"venue" json:"venue"`
 	EventID       int       `db:"event_id" form:"event_id" json:"event_id"`
 	EventDate     time.Time `db:"event_date" form:"event_date" json:"event_date"`
-	Content       int       `db:"content" form:"content" json:"content"`
+	Content       string       `db:"content" form:"content" json:"content"`
 	LastUpdaterID int       `db:"last_updater_id" form:"last_updater_id" json:"last_updater_id"`
 	CreatedAt     time.Time `db:"created_at" form:"created_at" json:"created_at"`
 	UpdatedAt     time.Time `db:"updated_at" form:"updated_at" json:"updated_at"`
