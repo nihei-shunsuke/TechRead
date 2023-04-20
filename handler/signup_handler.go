daf78d1117241bbbce95aafb324f7c4bba3dcf80
poteto <poteto054@gmail.com>
2023-04-20 16:24:29 +0900

    文章の簡略化

--- handler/signup_handler.go
+++ handler/signup_handler.go
@@ -14,32 +14,26 @@ func SignUpHandler(w http.ResponseWriter, req *http.Request) {
 	var resState model.ResInfo
 	//httpリクエスト(req.Body)に入ってるjsonデータ(model.User)を基にUser構造体にデコードする
 	if err := json.NewDecoder(req.Body).Decode(&reqUserData); err != nil {
-		fmt.Println(err)
-		http.Error(w, "fail to decode json\n", http.StatusBadRequest)
 		ResFail(resState)
 		json.NewEncoder(w).Encode(resState)
 		return
 	}
 	fmt.Println(reqUserData)
-	//User構造体にクライアントが記入したデータを入れる処理を書く、またはその関数を記述する//
+	//User構造体にリクエストが記入したデータを入れる処理を書く
 	const sqlStr = `
 		insert into users (user_name, password, email) values
 		(?,?,?);
 	`
-	//記入されたデータのuser_Emailに被りがないか調べる
+	//リクエストのメールアドレスに被りがないか調べる
 	row := database.DB.QueryRow("SELECT email FROM users WHERE email = ?", reqUserData.Email).Scan(&reqUserData.Email)
-	if row == sql.ErrNoRows {
-		//Exec文で戻り値としてレコードを期待しないクエリを実行する
-		//_をresultにすることで変更した行やデータを取得できる
+	if row == sql.ErrNoRows {//被りがない場合
 		result, err := database.DB.Exec(sqlStr, reqUserData.UserName, reqUserData.Password, reqUserData.Email)
 		if err != nil {
-			fmt.Println(err)
-			http.Error(w, "fail internal exec \n", http.StatusInternalServerError)
 			ResFail(resState)
 			json.NewEncoder(w).Encode(resState)
 			return
 		}
-		//ストリームにしてhttpレスポンスに出力している。
+		//ユーザーIDを追加された行数番号で登録している
 		id,_ := result.LastInsertId()
 		_, _ = database.DB.Exec("INSERT INTO users (user_id VALUES (?)", id)
 		resState.ResState = "success"
@@ -51,5 +45,4 @@ func SignUpHandler(w http.ResponseWriter, req *http.Request) {
 	resState.ResState = "failed"
 	resState.UserID = 0
 	json.NewEncoder(w).Encode(resState)
-	fmt.Println("このメールアドレスは使用されています")
 }
