9d10d091f47cc3c12d154b4570bb2944d250618a
Mana Kinoshita <73377985+HappyMana@users.noreply.github.com>
2023-05-02 16:01:21 +0900

    CreateChapter用のレスポンスの構造体を定義

--- model/response.go
+++ model/response.go
@@ -4,3 +4,8 @@ type ResInfo struct {
 	ResState  string   `db:"res_state" form:"res_state" json:"res_state"`
 	UserID    int64      `db:"user_id" form:"user_id" json:"user_id"`
 }
+
+type ResCreateChapter struct {
+	ResState  string   `db:"res_state" form:"res_state" json:"res_state"`
+	ChapterID int64    `db:"chapter_id" form:"res_state" json:"chapter_id"`
+}
