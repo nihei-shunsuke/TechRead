5b4b1a5c021a66a3ffaa5ddf792948afbbdee874
poteto <poteto054@gmail.com>
2023-05-02 17:21:11 +0900

    a

--- handler/resfunc.go
+++ handler/resfunc.go
@@ -4,13 +4,13 @@ import (
 	"TechRead/model"
 )
 
-func ResFail(m model.ResInfo){
+func ResFail(m model.ResInfo) {
 	m.ResState = "failed"
 	m.UserID = 0
 	return
 }
 
-func ResFailEvent(m model.ResCreateEvent){
+func ResFailEvent(m model.ResCreateEvent) {
 	m.ResState = "failed"
 	m.EventID = 0
 	return
