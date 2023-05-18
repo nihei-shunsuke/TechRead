02aaa5302b19375a929206827815f342e6b7a11e
poteto <poteto054@gmail.com>
2023-05-18 16:52:57 +0900

    今日の更新分 まだエラーある

--- handler/event_list_handler.go
+++ handler/event_list_handler.go
@@ -6,13 +6,14 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	//"strconv"
+	"strconv"
+	"log"
 )
 //リストでそのユーザーの参加した、してるイベントの詳細(イベントID、名前、本の名前、メンバーの数)を表示する
 //クッキーからuseridを取る→event-groupテーブルからeventidとuseridの数を取得→eventidからbooknameを取得
 type ResEventlist struct {
 	Status    string
-	event	  []eventdetail
+	event	  []*eventdetail
 }
 type eventdetail struct {
 	EventID		int
@@ -23,30 +24,36 @@ type eventdetail struct {
 func EventListHandler(w http.ResponseWriter, req *http.Request) {
 	var userRecord model.User
 	var resEvent ResEventlist
-	var loopNum int
-	//cookies := req.Cookies()
-	//if cookies != nil{
-	//	for _, c := range cookies {
-	//		ID,_ := strconv.ParseInt(c.Value, 10, 64)
-	//		fmt.Println(ID)
-	//		_ = database.DB.QueryRow("SELECT user_id FROM users WHERE user_id = ?", ID).Scan(&userRecord.UserID)
-	//	}
-	//}
-	userRecord.UserID = 1
-	rows,err := database.DB.Query("SELECT event_id FROM event_groups WHERE user_id = ? ",userRecord.UserID)
+	cookies := req.Cookies()
+	if cookies != nil{
+		for _, c := range cookies {
+			id, _ := strconv.ParseInt(c.Value, 10, 64)
+			fmt.Println(id)
+			_ = database.DB.QueryRow("SELECT user_id FROM users WHERE user_id = ?", id).Scan(&userRecord.UserID)
+		}
+	}
+	fmt.Println(userRecord.UserID)
+	// userRecord.UserID = 1
+	rows, err := database.DB.Query("SELECT event_id FROM event_groups WHERE user_id = ? ", userRecord.UserID)
 	if err != nil {
-		fmt.Println(err)
+		log.SetFlags(log.Lshortfile)
+		log.Println(err)
 	}
+	defer rows.Close()
+	fmt.Println("b")
+	eventNumArray := make([]int, 0)
 	for rows.Next() {
-		i := 0
-		fmt.Println(b)
-		rows.Scan(&resEvent.event[i].EventID)
-		i = i + 1
-		loopNum = i
+		var num int
+		err = rows.Scan(&num)
+		if err != nil {
+			log.SetFlags(log.Lshortfile)
+			log.Println(err)
+		}
+		eventNumArray = append(eventNumArray, num)
 	}
-	fmt.Println(c)
-	for i := 0; i < loopNum; i++ {
-		err := database.DB.QueryRow("SELECT book_name FROM events WHERE event_id = ?", resEvent.event[i].EventID).Scan(&resEvent.event[i].BookName)
+	fmt.Println("c")
+	for i := 0; i < len(eventNumArray); i++ {
+		err := database.DB.QueryRow("SELECT book_name FROM events WHERE event_id = ?", eventNumArray[i]).Scan(&resEvent.event[i].BookName)
 		if err != nil {
 			var resState model.ResInfo
 			fmt.Println(err)
@@ -54,7 +61,6 @@ func EventListHandler(w http.ResponseWriter, req *http.Request) {
 			json.NewEncoder(w).Encode(resState)
 			return
 		}
-		fmt.Println(d)
 		err = database.DB.QueryRow("SELECT COUNT ( user_id ) FROM event_groups WHERE event_id = ?", resEvent.event[i].EventID).Scan(&resEvent.event[i].MemberCount)
 		if err != nil {
 			var resState model.ResInfo
@@ -64,7 +70,6 @@ func EventListHandler(w http.ResponseWriter, req *http.Request) {
 			return
 		}
 	}
-	fmt.Println(e)
 	resEvent.Status = "success"
 	json.NewEncoder(w).Encode(resEvent)
 
